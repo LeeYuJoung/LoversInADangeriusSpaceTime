15605a38e97f6bcbb66441d19f6990b2ab50cff3
LeeYuJoung <nania000822@gmail.com>
2024-12-25 22:47:51 +0900

    Player Controller

--- Assets/02.Scripts/Player/PlayerController.cs
+++ Assets/02.Scripts/Player/PlayerController.cs
@@ -4,6 +4,17 @@ namespace yjlee
 {
     public class PlayerController : MonoBehaviour
     {
+        // 플레이어 상태
+        public enum STATE
+        {
+            Idle,      // 기본
+            Move,      // 이동
+            Jump,      // 점프
+            Crash,     // 충돌
+            Climb,     // 사다리
+            Operation  // 기계 조작
+        }
+
         private Rigidbody2D playerRigidbody;
 
         [SerializeField]
@@ -18,45 +29,7 @@ namespace yjlee
 
         void Update()
         {
-            Move();
-            Jump();
-            Action();
-        }
 
-        public void Move()
-        {
-            if (Input.GetKeyDown(KeyCode.RightArrow))
-            {
-
-            }
-            else if (Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-
-            }
-            else if (Input.GetKeyDown(KeyCode.UpArrow))
-            {
-
-            }
-            else if (Input.GetKeyDown(KeyCode.DownArrow))
-            {
-
-            }
-        }
-
-        public void Jump()
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                
-            }
-        }
-
-        public void Action()
-        {
-            if(Input.GetKeyDown(KeyCode.M))
-            {
-
-            }
         }
     }
 }
---
+++ Assets/02.Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+namespace yjlee
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        private void Update()
+        {
+            
+        }
+
+        public void Move()
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+
+            }
+            else if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+
+            }
+        }
+
+        public void Jump()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+
+            }
+        }
+
+        public void Action()
+        {
+            if (Input.GetKeyDown(KeyCode.M))
+            {
+
+            }
+        }
+    }
+}
