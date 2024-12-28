82c493c98b1846bd18b36c9545c9bcf92bce4e98
LeeYuJoung <nania000822@gmail.com>
2024-12-28 15:40:33 +0900

    Update playerController.cs

--- Assets/02.Scripts/AI/AStar/Grid.cs
+++ Assets/02.Scripts/AI/AStar/Grid.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 namespace yjlee.AI
 {
     // 설정한 x, y 기준으로 범위를 설정 후, 설정된 지름만큼 범위안에 노드를을 생성
-    public class AGrid : MonoBehaviour
+    public class Grid : MonoBehaviour
     {
 
     }
