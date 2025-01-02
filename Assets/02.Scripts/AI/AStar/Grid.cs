8f73bbc95c90da480a7ad7924155b1257a13b815
LeeYuJoung <nania000822@gmail.com>
2025-01-02 17:21:24 +0900

    Update Pet A* Algorithm

--- Assets/02.Scripts/AI/AStar/Grid.cs
+++ Assets/02.Scripts/AI/AStar/Grid.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 namespace yjlee.AI
 {    
     // 스크린을 Grid - Node로 분할
-    // 설정한 x, y 기준으로 범위를 설정 후, 설정된 지름만큼 범위안에 노드를을 생성
+    // 설정한 x, y 기준으로 범위를 설정 후, 설정된 지름만큼 범위안에 노드들을 생성
     public class Grid : MonoBehaviour
     {
         public Transform pet;
